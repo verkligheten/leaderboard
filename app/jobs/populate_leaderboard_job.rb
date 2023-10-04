3d7ef57d383339315ed663e5e2628ac7bea2f46f
Eugene Naumov <min.verkligheten@gmail.com>
2023-10-05 01:06:25 +0300

    Minor fix PopulateLeaderboardJob

--- app/jobs/populate_leaderboard_job.rb
+++ app/jobs/populate_leaderboard_job.rb
@@ -1,11 +1,12 @@
-require 'rake'
 
-Rails.application.load_tasks
 
 class PopulateLeaderboardJob < ApplicationJob
+  require 'rake'
+
   queue_as :default
 
   def perform(*args)
+    Rails.application.load_tasks
     Rake::Task['seed:leaderboard'].execute
   end
 end
