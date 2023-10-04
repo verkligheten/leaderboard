f8fc2ff9ebfc7d4445103996ce96c16a9e3ac32e
Eugene Naumov <min.verkligheten@gmail.com>
2023-10-04 23:41:31 +0300

    Add leaderboard to the root

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-
+    @users =
+      User.select(:id, :name, :country, :score)
+          .order(score: :desc)
+          .limit(100)
   end
 end
