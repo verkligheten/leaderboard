f4c9617b0cf64899bca5cce92d98af1dc044a607
Eugene Naumov <min.verkligheten@gmail.com>
2023-10-04 13:35:57 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
 end
