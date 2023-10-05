4b17273ae4270edc7149d493ef48601900bbec4d
Eugene Naumov <min.verkligheten@gmail.com>
2023-10-06 01:22:49 +0300

    Update update_users_score_container_job.rb

--- app/jobs/update_users_score_container_job.rb
+++ app/jobs/update_users_score_container_job.rb
@@ -2,7 +2,7 @@ class UpdateUsersScoreContainerJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    ids = User.pluck(:id).sample(6_000)
+    ids = User.order("RANDOM()").limit(6_000).pluck(:id)
 
     ids.in_groups_of(100, false) do |ids_batch|
       UpdateUsersScoreJob.perform_later(ids_batch)
