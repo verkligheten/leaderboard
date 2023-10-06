d9c26e75a1d86c248f38127504abf19e73a6db24
Eugene Naumov <min.verkligheten@gmail.com>
2023-10-06 05:03:34 +0300

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') { "redis://localhost:6379/1" } }
+  config.redis = { url: ENV.fetch('REDIS_URL') { "redis://localhost:6379/0" } }
 
   config.capsule("unsafe") do |cap|
     cap.concurrency = 1
@@ -8,5 +8,10 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') { "redis://localhost:6379/1" } }
+  config.redis = { url: ENV.fetch('REDIS_URL') { "redis://localhost:6379/0" } }
+end
+
+schedule_file = "config/schedule.yml"
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
